234d8975eafc5da8a61eebacc4b5f863957b03a6
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-09-29 13:20:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App/>
+);
 
-export default index;
